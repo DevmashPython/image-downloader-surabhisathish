50c860a686e4e5df0ff9c11689157f4f8a330d3b
surabhisathish <surabhisathish888@gmail.com>
2016-10-03 21:35:23 +0530

    surabhisathish

---
+++ urll.py
@@ -0,0 +1,11 @@
+import urllib
+import re
+url=raw.input("enter url:")
+f=urllib.urlopen(url)
+d=f.read()
+a=re.compile('<img.*src="(.*\.jpg)"')
+n=re.findall(a,d)
+b=open("link1.txt","w")
+for i in n:
+		b.write(url+i+"\n")
+b.close()
